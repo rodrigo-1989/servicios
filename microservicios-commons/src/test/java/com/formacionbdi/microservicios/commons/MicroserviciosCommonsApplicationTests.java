b5e073adacf40ea8488f220c6afd811e8de61366
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-13 16:34:13 -0500

    Agregando paqueteria commons

---
+++ microservicios-commons/src/test/java/com/formacionbdi/microservicios/commons/MicroserviciosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
