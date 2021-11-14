7584b35c6a017b51f22c268ba3ea229c05ccdc69
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-13 19:48:58 -0500

    Agregado de metodos para agregar o eliminar alumnos  del curso

--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -1,17 +1,23 @@
 package com.formacionbdi.microservicios.app.cursos.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 
 @Entity
 @Table(name = "cursos")
@@ -25,11 +31,18 @@ public class Curso  implements Serializable{
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 	
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Alumno> alumnos;
+	
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
 	} 
 	
+	public Curso() {
+		this.alumnos = new ArrayList<>();
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -47,6 +60,22 @@ public class Curso  implements Serializable{
 	}
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+	
+	public void addAlumnos(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
+	
+	public void removeAlumnos(Alumno alumno) {
+		this.alumnos.add(alumno);
 	}
 	/**
 	 * 
