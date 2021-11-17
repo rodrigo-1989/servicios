fdc8f17d77a0153260324e1c8d848de51e1ad6bc
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-16 23:41:04 -0500

    Agregado del metodo editar en el controlador

--- microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
+++ microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
@@ -1,5 +1,9 @@
 package com.formacionbdi.microservicios.app.examenes.controllers;
 
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +19,21 @@ public class ExamenController extends CommonController<Examen, ExamenService>{
 
 	@PutMapping
 	public ResponseEntity<?> editar(@RequestBody Examen examen,@PathVariable Long id ){
-		return null;
+		Optional <Examen> o = service.findById(id);
+		
+		if(!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Examen examenDb =o.get();
+		examenDb.setNombre(examen.getNombre());
+		
+
+		examenDb.getPreguntas()
+		.stream().filter(pdb -> !examen.getPreguntas().contains(pdb))
+		.forEach(examenDb::removePregunta);
+		
+		examenDb.setPreguntas(examen.getPreguntas());
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(examenDb));
 	}
 }
